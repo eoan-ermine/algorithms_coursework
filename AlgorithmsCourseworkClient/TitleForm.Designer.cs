057f1c827cea314abef427965f81901a17648b98
PatriotRossii <patriotrossii2019@mail.ru>
2023-05-14 23:28:50 +0300

    Update TitleForm.Designer.cs

--- AlgorithmsCourseworkClient/TitleForm.Designer.cs
+++ AlgorithmsCourseworkClient/TitleForm.Designer.cs
@@ -40,12 +40,12 @@
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.theoryButton = new System.Windows.Forms.Button();
             this.testButton = new System.Windows.Forms.Button();
+            this.playerButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.timeLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.versionLabel = new System.Windows.Forms.ToolStripStatusLabel();
-            this.playerButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
@@ -55,9 +55,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(17, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(609, 48);
+            this.label1.Size = new System.Drawing.Size(783, 58);
             this.label1.TabIndex = 0;
             this.label1.Text = "МИНИСТЕРСТВО ЦИФРОВОГО РАЗВИТИЯ, СВЯЗИ И МАССОВЫХ\r\nКОММУНИКАЦИЙ РОССИЙСКОЙ ФЕДЕРА" +
     "ЦИИ";
@@ -66,9 +67,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(14, 70);
+            this.label2.Location = new System.Drawing.Point(19, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(689, 96);
+            this.label2.Size = new System.Drawing.Size(876, 116);
             this.label2.TabIndex = 1;
             this.label2.Text = "Ордена Трудового Красного Знамени\r\nФедеральное государственное бюджетное образова" +
     "тельное учреждение\r\nвысшего образования\r\n«Московский технический университет свя" +
@@ -78,9 +80,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(498, 189);
+            this.label3.Location = new System.Drawing.Point(664, 233);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(238, 24);
+            this.label3.Size = new System.Drawing.Size(309, 29);
             this.label3.TabIndex = 2;
             this.label3.Text = "Кафедра «Информатика»";
             // 
@@ -88,9 +91,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(284, 248);
+            this.label4.Location = new System.Drawing.Point(379, 305);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(668, 78);
+            this.label4.Size = new System.Drawing.Size(820, 93);
             this.label4.TabIndex = 3;
             this.label4.Text = "Курсовая работа\r\nпо дисциплине «Алгоритмы и алгоритмические языки» на тему:\r\nЭксп" +
     "луатация корпоративных информационных систем";
@@ -100,9 +104,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(768, 368);
+            this.label5.Location = new System.Drawing.Point(1024, 453);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(344, 48);
+            this.label5.Size = new System.Drawing.Size(445, 58);
             this.label5.TabIndex = 4;
             this.label5.Text = "Выполнил: студент группы БФИ-2202\r\nСидорук Д. В.";
             // 
@@ -110,9 +115,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(768, 425);
+            this.label6.Location = new System.Drawing.Point(1024, 523);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(383, 72);
+            this.label6.Size = new System.Drawing.Size(494, 87);
             this.label6.TabIndex = 5;
             this.label6.Text = "Научный руководитель: старший\r\nпреподаватель кафедры «Информатика»\r\nЗагвоздкина А" +
     ". В";
@@ -121,18 +127,20 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(498, 512);
+            this.label7.Location = new System.Drawing.Point(664, 630);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(146, 24);
+            this.label7.Size = new System.Drawing.Size(184, 29);
             this.label7.TabIndex = 6;
             this.label7.Text = "Москва, 2023 г.";
             // 
             // label8
             // 
             this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label8.Location = new System.Drawing.Point(0, 550);
+            this.label8.Location = new System.Drawing.Point(0, 677);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(1179, 2);
+            this.label8.Size = new System.Drawing.Size(1572, 2);
             this.label8.TabIndex = 7;
             // 
             // tableLayoutPanel1
@@ -141,11 +149,12 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 552);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 679);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1163, 49);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1551, 60);
             this.tableLayoutPanel1.TabIndex = 9;
             // 
             // flowLayoutPanel1
@@ -157,18 +166,20 @@
             this.flowLayoutPanel1.Controls.Add(this.testButton);
             this.flowLayoutPanel1.Controls.Add(this.playerButton);
             this.flowLayoutPanel1.Controls.Add(this.exitButton);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(272, 3);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(259, 4);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(618, 43);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1033, 52);
             this.flowLayoutPanel1.TabIndex = 10;
             // 
             // theoryButton
             // 
             this.theoryButton.AutoSize = true;
             this.theoryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.theoryButton.Location = new System.Drawing.Point(3, 3);
+            this.theoryButton.Location = new System.Drawing.Point(4, 4);
+            this.theoryButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.theoryButton.Name = "theoryButton";
-            this.theoryButton.Size = new System.Drawing.Size(232, 34);
+            this.theoryButton.Size = new System.Drawing.Size(393, 48);
             this.theoryButton.TabIndex = 0;
             this.theoryButton.Text = "Электронный конспект";
             this.theoryButton.UseVisualStyleBackColor = true;
@@ -179,21 +190,36 @@
             this.testButton.AutoSize = true;
             this.testButton.Dock = System.Windows.Forms.DockStyle.Left;
             this.testButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.testButton.Location = new System.Drawing.Point(241, 3);
+            this.testButton.Location = new System.Drawing.Point(405, 4);
+            this.testButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.testButton.Name = "testButton";
-            this.testButton.Size = new System.Drawing.Size(150, 34);
+            this.testButton.Size = new System.Drawing.Size(253, 48);
             this.testButton.TabIndex = 2;
             this.testButton.Text = "Тестирование";
             this.testButton.UseVisualStyleBackColor = true;
             this.testButton.Click += new System.EventHandler(this.testButton_Click);
             // 
+            // playerButton
+            // 
+            this.playerButton.AutoSize = true;
+            this.playerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.playerButton.Location = new System.Drawing.Point(666, 4);
+            this.playerButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.playerButton.Name = "playerButton";
+            this.playerButton.Size = new System.Drawing.Size(228, 48);
+            this.playerButton.TabIndex = 3;
+            this.playerButton.Text = "Медиаплеер";
+            this.playerButton.UseVisualStyleBackColor = true;
+            this.playerButton.Click += new System.EventHandler(this.playerButton_Click);
+            // 
             // exitButton
             // 
             this.exitButton.AutoSize = true;
             this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.exitButton.Location = new System.Drawing.Point(537, 3);
+            this.exitButton.Location = new System.Drawing.Point(902, 4);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(78, 34);
+            this.exitButton.Size = new System.Drawing.Size(127, 48);
             this.exitButton.TabIndex = 1;
             this.exitButton.Text = "Выход";
             this.exitButton.UseVisualStyleBackColor = true;
@@ -201,53 +227,43 @@
             // 
             // statusStrip1
             // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.timeLabel,
             this.toolStripStatusLabel1,
             this.versionLabel});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 604);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 744);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1163, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1551, 26);
             this.statusStrip1.TabIndex = 10;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // timeLabel
             // 
             this.timeLabel.Name = "timeLabel";
-            this.timeLabel.Size = new System.Drawing.Size(79, 17);
+            this.timeLabel.Size = new System.Drawing.Size(102, 20);
             this.timeLabel.Text = "Дата и время";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(993, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(1333, 20);
             this.toolStripStatusLabel1.Spring = true;
             // 
             // versionLabel
             // 
             this.versionLabel.Name = "versionLabel";
             this.versionLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.versionLabel.Size = new System.Drawing.Size(76, 17);
+            this.versionLabel.Size = new System.Drawing.Size(96, 20);
             this.versionLabel.Text = "Версия: 1.0.0";
             this.versionLabel.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             // 
-            // playerButton
-            // 
-            this.playerButton.AutoSize = true;
-            this.playerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.playerButton.Location = new System.Drawing.Point(397, 3);
-            this.playerButton.Name = "playerButton";
-            this.playerButton.Size = new System.Drawing.Size(134, 34);
-            this.playerButton.TabIndex = 3;
-            this.playerButton.Text = "Медиаплеер";
-            this.playerButton.UseVisualStyleBackColor = true;
-            this.playerButton.Click += new System.EventHandler(this.playerButton_Click);
-            // 
             // TitleForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1163, 626);
+            this.ClientSize = new System.Drawing.Size(1551, 770);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.label8);
@@ -258,6 +274,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "TitleForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Титульный лист — курсовая работа — Сидорук Данил Вадимович, БФИ-2202";
